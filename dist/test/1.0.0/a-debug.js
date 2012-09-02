2cab49319e1d14bf24a0e45f998e920ce97b0f77
aSEioT <aseiot@hotmail.com>
2012-09-02 23:36:37 +0800

    generate error module name when module file name is ending with '.js'

--- dist/test/1.0.0/a-debug.js
+++ dist/test/1.0.0/a-debug.js
@@ -1,22 +1,22 @@
 define("test/1.0.0/b-debug", [], function(require, exports) {
-	var say=function(word){
-		alert(word+"__defined in b!")
-	}
+    var say=function(word){
+        alert(word+"__defined in b!")
+    }
 
 	exports.say=say;
 })
 
 define("test/1.0.0/c-debug", ["./b.js-debug"], function(require, exports) {
-	var say = function(){
-		require('./b.js-debug').say("from c")
-	}
+    var say = function(){
+	    require('./b.js-debug').say("from c")
+    }
 	exports.say=say;
 })
 
 define("test/1.0.0/a-debug", ["./b.js-debug", "./c.js-debug"], function(require, exports, module){
 	var init=function(){
 //	 require('./b-debug').say("from a");
-		require('./c.js-debug').say();
+	 require('./c.js-debug').say();
 	}
 	exports.init=init;
 })
